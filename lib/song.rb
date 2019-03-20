cd6d07a193d46891c15c8018f69145169ea6f006
Daniel Schneider <daschne8@gmail.com>
2019-03-20 14:54:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,7 @@ class Song
     self.all.find{|song| song.name == name}
   end
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) != false
-      return self.find_by_name(name)
-    else
-      return self.create_by_name(name)
-    end
+    self.find_by_name(name) ||= self.create_by_name(name)
   end
 end
 
