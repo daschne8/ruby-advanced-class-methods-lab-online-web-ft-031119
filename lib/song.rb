a1fb2a9d2b6b72321613d4a7758335dcdf944e35
Daniel Schneider <daschne8@gmail.com>
2019-03-20 14:06:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,12 @@ class Song
     self.all << song
     song
   end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
   def self.all
     @@all
   end
